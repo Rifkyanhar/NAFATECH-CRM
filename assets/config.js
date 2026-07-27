3511297355e4fbfa2b60e6f4ab823a028e26f277
Rifkyanhar <rifkymuslim83@gmail.com>
2026-07-27 18:35:54 +0700

    Fix syntax error in config.js

--- assets/config.js
+++ assets/config.js
@@ -6,7 +6,7 @@
 
 // 1. Ambil dari Supabase Dashboard > Project Settings > API
 window.NAFA_CONFIG = {
-  SUPABASE_URL:  "https://uuejlrebqanlkgguerhk.supabase.co",     ,
+  SUPABASE_URL:  "https://uuejlrebqanlkgguerhk.supabase.co",
   SUPABASE_ANON_KEY:  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV1ZWpscmVicWFubGtnZ3VlcmhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNDI3MTIsImV4cCI6MjEwMDcxODcxMn0.CnlC0xknSICXYv1hxCEcqwsSYP8N6QVXHm_Z_xsKluY",
 
   // 2. Ambil dari akun EmailJS (emailjs.com) — untuk kirim notifikasi
